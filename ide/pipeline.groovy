a75112a49cba28777c6b1dea31b9f2c40846f88a
nagkraju <33889214+nagkraju@users.noreply.github.com>
2021-08-03 09:45:28 +0530

    pipeline code from the Jenkins WebUI copied to pipeline.groovy in vscode

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,35 @@
 pipeline {
-    agent any
-    triggers { pollSCM('* * * * *') }
+  
+  agent any
+  
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
-        }
+                // Get some code from a GitHub repository
+                git url:'https://github.com/nagkraju/jgsu-spring-petclinic.git', branch: 'main'
+            }
+        } 
+        
         stage('Build') {
             steps {
+                // Get some code from a GitHub repository
+               // git url:'https://github.com/nagkraju/jgsu-spring-petclinic.git', branch: 'main'
+
                 sh './mvnw clean package'
-                //sh 'false' // true
+
+                // Run Maven on a Unix agent.
+               // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
-        
+
             post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
