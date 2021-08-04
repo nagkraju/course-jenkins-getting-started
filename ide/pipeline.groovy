c7a389af43c239627b67d59b4620751a8075a07a
nagkraju <33889214+nagkraju@users.noreply.github.com>
2021-08-04 10:23:30 +0530

    .vscode settings updated to trigger builds from within VS Code

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,7 +1,7 @@
 pipeline {
   
   agent any
-  
+  triggers{ pollSCM('* * * * *') }
     stages {
         stage('Checkout') {
             steps {
@@ -30,7 +30,13 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
+               // }
+                //changed {
+                    
+                emailext body: 'Please go to ${BUILD_URL} and verify the build', 
+                subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input',
+                    to: 'nagkraju@yahoo.com'
+               }
             }
         }
     }
